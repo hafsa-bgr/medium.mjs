b8073edc24c3ad055c2a3686b1204d4ed238adfc
hafsa-bgr <hafsa.boughir@gmail.com>
2024-09-26 18:58:36 +0200

    Create medium.mjs

---
+++ medium.mjs
@@ -0,0 +1,12 @@
+let getal1 = "33";  
+let getal2 = 7;     
+
+let som = getal1 + getal2;          
+let verschil = getal1 - getal2;     
+let product = getal1 * getal2;      
+let deling = getal1 / getal2;       
+
+console.log("Som: " + som);
+console.log("Verschil: " + verschil);
+console.log("Product: " + product);
+console.log("Deling: " + deling);
